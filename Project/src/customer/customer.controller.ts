6faa428deeffc52550777e12402c54c1c9221ec2
Owasi <mohammadowasi70@gmail.com>
2024-03-24 04:39:42 +0600

    swagger

--- Project/src/customer/customer.controller.ts
+++ Project/src/customer/customer.controller.ts
@@ -5,42 +5,41 @@ import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { SetSchemaGuard } from 'src/guards/schema.guard';
 import { RoleGuard } from 'src/guards/role.guard';
+import { ApiTags, ApiBearerAuth, ApiOkResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiBadRequestResponse } from '@nestjs/swagger'; // Add this line
 
+@ApiTags('Customer') 
 @Controller('customer')
-@UseGuards(JwtAuthGuard,SetSchemaGuard,new RoleGuard(['owner','salesman']))
+@UseGuards(JwtAuthGuard, SetSchemaGuard, new RoleGuard(['owner', 'salesman']))
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
+  @ApiBearerAuth('access-token') 
+  @ApiCreatedResponse({ description: 'The customer has been successfully created.' }) 
   @Post('/create')
   async createCustomerProfile(@Body(ValidationPipe) createCustomerDto: CreateCustomerDto) {
     return this.customerService.createCustomerProfile(createCustomerDto);
   }
 
+  @ApiBearerAuth() 
+  @ApiOkResponse({ description: 'List of all customers.' }) 
   @Get()
   findAll() {
     return this.customerService.findAll();
   }
 
+  @ApiBearerAuth() 
+  @ApiOkResponse({ description: 'Customer found successfully.' }) 
+  @ApiNotFoundResponse({ description: 'Customer not found.' }) 
   @Get('/:contact')
   findOne(@Param('contact') contact: string) {
     return this.customerService.findOne(contact);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-  //   return this.customerService.update(+id, updateCustomerDto);
-  // }
-
-  
-
-@Patch('/update/:contact')
-async updateCustomer(@Param('contact') contact: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-  return this.customerService.updateCustomer(contact, updateCustomerDto);
-}
-
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.customerService.remove(+id);
-  // }
+  @ApiBearerAuth() 
+  @ApiOkResponse({ description: 'Customer updated successfully.' }) 
+  @ApiBadRequestResponse({ description: 'Bad request, invalid data provided.' }) 
+  @Patch('/update/:contact')
+  async updateCustomer(@Param('contact') contact: string, @Body() updateCustomerDto: UpdateCustomerDto) {
+    return this.customerService.updateCustomer(contact, updateCustomerDto);
+  }
 }
