6230fad6918ce95e0559f1ad31bd02a7d8d270eb
ashrafulatif <ashrafulhaque08@gmail.com>
2024-05-12 02:45:32 +0600

    change rootLayout metadata

--- Project/dist/src/main.js
+++ Project/dist/src/main.js
@@ -7,6 +7,7 @@ const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     const config = new swagger_1.DocumentBuilder()
         .setTitle('All in One Business Solution')
         .setDescription('All in one business solution API description')
--- Project/src/main.ts
+++ Project/src/main.ts
@@ -6,6 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('All in One Business Solution')
