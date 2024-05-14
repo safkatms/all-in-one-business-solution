a86dff822505fd82c0dff9ab9bf3a3b99f3f6882
Safkat Mahmud Sakib <safkatmahmudsakib@gamil.com>
2024-05-14 21:58:22 +0600

    Create Payroll

--- frontend/src/components/employeeTable.tsx
+++ frontend/src/components/employeeTable.tsx
@@ -85,7 +85,7 @@ export default function EmployeeTable() {
 
   return (
     <ProtectedRoute requiredRole={["owner","hr"]}>
-      <div className="flex justify-center mt-8">
+      <div className="flex justify-center p-4">
         <div className="w-100%">
           <h1 className="text-2xl text-left mb-2"> All Products:</h1>
           <form onSubmit={handleSubmit}>
