17e0144ceab2b4c207ee3de2366d8d1e8391b829
owasi70 <mohammadowasi70@gmail.com>
2024-05-14 21:59:43 +0600

    Customer Done

---
+++ frontend/src/app/customer/update/[contact]/page.tsx
@@ -0,0 +1,258 @@
+"use client";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import InsideHeader from "@/components/insideheader";
+import Sidebar from "@/components/sidebar";
+import ProtectedRoute from "@/utils/protectedRoute";
+import { getToken } from "@/utils/auth";
+import Cookies from "js-cookie";
+import CustomerListTable from '@/components/customerlisttable';
+import CustomerSearch from '@/components/customersearch';
+
+interface Customer {
+  name: string;
+  contact: string;
+  email: string;
+ 
+}
+
+export default function UpdateCustomer({
+  params,
+}: {
+  params: { contact: string };
+}) {
+  const {contact } = params;
+  const [customer, setCustomer] = useState<Customer>({
+    
+    name: "",
+    contact: "",
+    email: "",
+   
+  });
+
+
+  const [name, setname] = useState("");
+  const [contactNo, setcontact] = useState("");
+  const [email, setemail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChangename = (e: ChangeEvent<HTMLInputElement>) => {
+    setname(e.target.value);
+  };
+
+  const handleChangecontact = (e: ChangeEvent<HTMLInputElement>) => {
+    setcontact(e.target.value);
+  };
+
+  const handleChangeemail = (e: ChangeEvent<HTMLInputElement>) => {
+    setemail(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchCustomerDetails = async () => {
+      try {
+        const token = Cookies.get("jwtToken");
+        const response = await axios.get<Customer>(
+          `http://localhost:3000/customer/${contact}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setCustomer(response.data);
+
+        setname(response.data.name);
+        setcontact(response.data.contact);
+        setemail(response.data.email);
+        
+      } catch (error) {
+        console.error("Error fetching customer details:", error);
+      }
+    };
+
+    fetchCustomerDetails();
+  }, [contactNo]);
+
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log("Submit button clicked!");
+    console.log("Form data:", {
+      name,
+      contact,
+      email,
+      
+    });
+
+    setError("");
+    if (
+      !name ||
+      !contact ||
+      !email
+      
+    ) {
+      setError("All fields are required");
+      return;
+    }
+     
+    if (!name.match(/^[A-Z][a-z]*$/)) {
+      setError(
+        "Customer name must start with a capital letter and contain no numbers or special characters."
+      );
+      return;
+    }
+    if (!email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+      setError("Invalid email address.");
+      return;
+    }
+    
+    if (!contact.match(/^01[3-9]\d{8}$/)) {
+      setError("Mobile number must be a valid Bangladesh number.");
+      return;
+    }
+    
+    try {
+      const response = await postData();
+      console.log("Customer Update successfully:", response);
+      setError("Customer Updated successfully");
+      
+      setname("");
+      setcontact("");
+      setemail("");
+    } catch (error) {
+      console.error("Error creating customer:", error);
+      setError("Customer Already Exist");
+    }
+  };
+
+  async function postData() {
+    try {
+      const token = getToken();
+      const data1 = {
+        name: name,
+        contact: contact,
+        email: email,
+      };
+      const response = await axios.patch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/customer/update/${contact}`,
+        data1,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      throw new Error("Error creating customer");
+    }
+  }
+
+
+
+
+
+
+
+
+
+  return (
+
+<ProtectedRoute requiredRole={["owner"]}>
+      <InsideHeader />
+      <div className="flex">
+        <Sidebar />
+        <div className="bg-white my-10 w-screen h-fit shadow-2xl rounded-xl">
+          <h1 className="text-4xl font-extrabold flex justify-center p-8">
+           Customer Update
+          </h1>
+          {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+          <div className="p-8 flex justify-center">
+            <form onSubmit={handleSubmit}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td colSpan={2}>
+                      <label className="text-lg">Name</label>
+                    </td>
+                   
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                      <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleChangename}
+                        className="bg-customGray rounded w-full py-2 px-3 text-customBlack2 leading-tight focus:outline-none focus:shadow-outline"
+                      />
+                    </td>
+                   
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                      <label>Contact</label>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                      <input
+                        type="text"
+                        name="contact"
+                        value={contact}
+                        onChange={handleChangecontact}
+                        className="bg-customGray rounded w-full py-2 px-3 text-customBlack2 leading-tight focus:outline-none focus:shadow-outline"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                      <label className="text-lg">Email</label>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                      <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChangeemail}
+                        className="bg-customGray rounded w-full py-2 px-3 text-customBlack2 leading-tight focus:outline-none focus:shadow-outline"
+                      />
+                    </td>
+                  </tr>
+                  <tr className="text-center">
+                    <td>
+                      <button
+                        type="submit"
+                        className="bg-customTeal text-white rounded-lg font-semibold w-mid mt-2 py-2 px-3"
+                      >
+                        Update
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="reset"
+                        className="bg-red-600 text-white rounded-lg font-semibold w-mid mt-2 py-2 px-3"
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </div>
+          <CustomerListTable/>
+        </div>
+      </div>
+    </ProtectedRoute>
+    
+  );
+};
+
--- frontend/src/components/customerlisttable.tsx
+++ frontend/src/components/customerlisttable.tsx
@@ -1,28 +1,117 @@
+"use client";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import { getToken } from "@/utils/auth";
+import ConfirmationModal from "./ConfirmationModal";
+import ProtectedRoute from "@/utils/protectedRoute";
+
+interface Customer {
+  name: string;
+  contact: string;
+  email: string;
+  
+}
+
 
 export default function CustomerListTable() {
-    return (
-      <>
-        <div className="flex justify-center mt-8">
-          <div className="w-100%">
-            <h1 className="text-2xl text-center  mb-2"> Customer List</h1>
+   
+  const [customer, setCustomer] = useState<Customer[]>([]);
+  const [error, setError] = useState("");
+  const [contact, setcontact] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  
+  const handleChangecontact = (contact: any) => {
+    setcontact(contact);
+  };
+
+    useEffect(() => {
+      const fetchCustomer = async () => {
+        try {
+          const token = getToken();
+          const response = await axios.get(
+            `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/customer`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setCustomer(response.data);
+        } catch (error) {
+          console.error("Error fetching customer:", error);
+        }
+      };
+      fetchCustomer();
+    }, []);
+
+    const router = useRouter();
+    
+    const handleUpdate = (contact: string) => {
+      router.push(`/customer/update/${contact}`);
+    };
+
+
+
+  async function postData() {
+    try {
+      const token = Cookies.get("jwtToken");
+      const response = await axios.patch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/customer/update/contact${contact}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <ProtectedRoute requiredRole={["owner","hr"]}>
+    
+      <div className="flex justify-center mt-8">
+        <div className="w-100%">
+          <h1 className="text-2xl text-left mb-2"> All Customer:</h1>
             <table className="min-w-full bg-white rounded-lg overflow-hidden text-sm">
-              <thead className="bg-customBlack2 text-white">
+              <thead className="bg-gray-600 text-white">
                 <tr>
-                  <th className="px-4 py-2">ID</th>
-                  <th className="px-4 py-2">Name</th>
+                  <th className="px-4 py-2">Customer Name</th>
                   <th className="px-4 py-2">Contact</th>
-                  <th className="px-4 py-2">E-mail</th>
+                  <th className="px-4 py-2">Email</th>
                   <th className="px-4 py-2">Action</th>
                 </tr>
               </thead>
               <tbody className="text-gray-700">
-                
+                {customer.map((customer) => (
+                  <tr key={customer.contact}>
+                    <td className="px-4 py-2">{customer.name}</td>
+                    <td className="px-4 py-2">{customer.contact}</td>
+                    <td className="px-4 py-2">{customer.email}</td>
+                    
+                    <td className="px-4 py-2">
+                      <button
+                        type="submit"
+                        className="bg-customTeal hover:bg-customBlack2 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline w-full sm:w-auto"
+                        value={customer.contact}
+                        onClick={() =>
+                          handleUpdate(customer.contact)
+                        }
+                      >
+                        Update
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
-          </div>
-          
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </ProtectedRoute>
+  );
+}
